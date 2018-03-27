4d13f15907d98d3fd384be1af5058f530658a0a2
yossy3459 <yoshi3459@outlook.com>
2018-03-27 14:20:55 +0900

    initial commit

---
+++ Kilo.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Ateam
+{
+    public class Kilo : BaseBattleAISystem
+    {
+        //---------------------------------------------------
+        // InitializeAI
+        //---------------------------------------------------
+        override public void InitializeAI()
+        {
+        }
+
+        //---------------------------------------------------
+        // UpdateAI
+        //---------------------------------------------------
+        override public void UpdateAI()
+        {
+        }
+
+        //---------------------------------------------------
+        // ItemSpawnCallback
+        //---------------------------------------------------
+        override public void ItemSpawnCallback(ItemSpawnData itemData)
+        {
+        }
+    }
+}
