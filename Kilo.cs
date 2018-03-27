1983ca0a092473075a42f92900cdc6944074aa5a
yossy3459 <yoshi3459@outlook.com>
2018-03-27 15:02:40 +0900

    テンプレート作成

--- Kilo.cs
+++ Kilo.cs
@@ -6,11 +6,28 @@ namespace Ateam
 {
     public class Kilo : BaseBattleAISystem
     {
+
+        public List<CharacterModel.Data> playerList;
+        public List<CharacterModel.Data> enemyList;	
+        public int[,] stageData;
+        public Vector2 stageDataBlockNum;
         //---------------------------------------------------
         // InitializeAI
         //---------------------------------------------------
         override public void InitializeAI()
         {
+            // 取得
+            playerList = GetTeamCharacterDataList(TEAM_TYPE.PLAYER);
+            enemyList = GetTeamCharacterDataList(TEAM_TYPE.ENEMY);
+            stageDataBlockNum = GetStageDataBlockNum();
+            stageData = GetStageData();
+
+            // デバッグ
+            Debug.Log(playerList);
+            Debug.Log(enemyList);
+            Debug.Log(stageDataBlockNum);
+            Debug.Log(stageData);
+
         }
 
         //---------------------------------------------------
@@ -18,6 +35,20 @@ namespace Ateam
         //---------------------------------------------------
         override public void UpdateAI()
         {
+            foreach (var player in playerList)
+            {
+                // 全プレイヤーの処理
+                Debug.Log(player.ActorId);
+
+                // player[0]の処理
+                if (player.ActorId == 0)
+                {
+                    
+                }
+
+
+
+            }
         }
 
         //---------------------------------------------------
